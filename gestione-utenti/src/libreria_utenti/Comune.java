16959370de55bca1c710cec9ac7ad8e2b2ba0fbd
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2026-04-16 07:41:10 +0000

    FIX: Eliminato controllo date

--- gestione-utenti/src/libreria_utenti/Comune.java
+++ gestione-utenti/src/libreria_utenti/Comune.java
@@ -1,15 +1,9 @@
 package libreria_utenti;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class Comune {
     private String sigla_provincia;
     private String denominazione_ita;
     private String codice_belfiore;
-    private LocalDate data_inizio_validita;
-    private LocalDate data_fine_validita;
-    private DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public Comune(String rigaCSV) throws Exception {
         // PD;ABANO;A001;19/11/1866;13/11/1924
@@ -23,36 +17,15 @@ public class Comune {
             this.denominazione_ita = tmp[1].replace("\"", "");
             this.codice_belfiore = tmp[2];
 
-            // Controllo se ci sono le date...
-
-            if (tmp.length > 3) {
-                this.data_inizio_validita = LocalDate.parse(tmp[3], this.formatoData);
-            }
-            if (tmp.length > 4) {
-                this.data_fine_validita = LocalDate.parse(tmp[4], this.formatoData);
-            }
         } catch (Exception e) {
             throw new Exception("Errore durante la creazione dell'oggetto. " + e.getMessage());
         }
     }
 
-    public Comune(String sigla_provincia, String denominazione_ita, LocalDate data) throws Exception {
-        try {             
+    public Comune(String sigla_provincia, String denominazione_ita) {
+
             this.sigla_provincia = sigla_provincia;
             this.denominazione_ita = denominazione_ita.replace("\"", "");
-            this.data_inizio_validita = data;
-        } catch (Exception e) {
-            throw new Exception("Errore durante la creazione dell'oggetto. " + e.getMessage());
-        }
-    }
-
-    public Comune(String sigla_provincia, String denominazione_ita, String data) throws Exception {
-        try {
-            this.denominazione_ita = denominazione_ita.replace("\"", "");
-            this.data_inizio_validita = LocalDate.parse(data, this.formatoData);
-        } catch (Exception e) {
-            throw new Exception("Errore durante la creazione dell'oggetto. " + e.getMessage());
-        }
     }
 
     public String getCodiceComune() {
@@ -61,34 +34,27 @@ public class Comune {
 
     @Override
     public String toString() {
-        String tmp = this.sigla_provincia + ";" + this.denominazione_ita + ";" + this.codice_belfiore;
-        tmp += this.data_inizio_validita != null ? ";" + this.data_inizio_validita.format(this.formatoData) : "";
-        tmp += this.data_fine_validita != null ? ";" + this.data_fine_validita.format(this.formatoData) : "";
-        return tmp;
+        return this.sigla_provincia + ";" + this.denominazione_ita + ";" + this.codice_belfiore;        
     }
 
     @Override
     public boolean equals(Object obj) {
 
-        if (! (obj.getClass() == Comune.getClass())) {
+        if (obj == null) {
             return false;
         }
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
         Comune tmp = (Comune) obj;
 
-        if (this.data_inizio_validita == null) {
-            return this.denominazione_ita.toUpperCase().equals(tmp.denominazione_ita.toUpperCase()) &&
-            this.sigla_provincia.toUpperCase().equals(tmp.sigla_provincia.toUpperCase());
-        } else {
-            if (this.data_fine_validita == null) {
-                return this.denominazione_ita.toUpperCase().equals(tmp.denominazione_ita.toUpperCase()) &&
-                       this.sigla_provincia.toUpperCase().equals(tmp.sigla_provincia.toUpperCase()) &&
-                       tmp.data_inizio_validita.isAfter(this.data_inizio_validita) || tmp.data_inizio_validita.equals(this.data_inizio_validita);
-            } else {
-                return ( this.denominazione_ita.toUpperCase().equals(tmp.denominazione_ita.toUpperCase()) &&
-                         this.sigla_provincia.toUpperCase().equals(tmp.sigla_provincia.toUpperCase()) &&
-                        tmp.data_inizio_validita.isAfter(this.data_inizio_validita) || tmp.data_inizio_validita.equals(this.data_inizio_validita)) &&
-                        (tmp.data_inizio_validita.isBefore(this.data_fine_validita) || tmp.data_inizio_validita.equals(this.data_fine_validita));
-            }
-        }
+        return this.denominazione_ita.toUpperCase().equals(tmp.denominazione_ita.toUpperCase()) &&
+               this.sigla_provincia.toUpperCase().equals(tmp.sigla_provincia.toUpperCase());
     }
 }
